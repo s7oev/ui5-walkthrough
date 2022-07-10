21f903addb0170da97ac1362d6625e7e60ff8bd5
Stoyko Stoev <stoyko.stoev@sap.com>
2022-07-10 12:06:50 +0000

    nested views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,19 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.App", {
-        onShowHello: function () {
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            MessageToast.show(sMsg);
-        }
-    });
+    return Controller.extend("sap.ui.demo.walkthrough.App", {});
 });
