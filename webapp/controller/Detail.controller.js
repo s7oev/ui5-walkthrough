84388dc4db36511e2d0ca29637749575a42f823a
Stoyko Stoev <stoyko.stoev@sap.com>
2022-07-23 11:58:08 +0000

    routing back and history

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History"
+], function (Controller, UIComponent, History) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
         onInit: function () {
-            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            const oRouter = UIComponent.getRouterFor(this);
             oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
 
@@ -15,6 +16,18 @@ sap.ui.define([
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
             })
+        },
+
+        onNavBack: function () {
+            const oHistory = History.getInstance();
+            const sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                const oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("overview", {}, true);
+            }
         }
     });
 });
